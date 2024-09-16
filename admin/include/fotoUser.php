8468c9fbaf0481fa5d9acc3d89719d67df23a53a
jesse-barbosa <barbosajesse419@gmail.com>
2024-09-16 11:16:32 -0300

    Easter egg

--- admin/include/fotoUser.php
+++ admin/include/fotoUser.php
@@ -1,8 +1,18 @@
 <div class="col bg-warning">
     <div class="d-flex flex-row text-center">
-        <img src="../img/icon.png" class="rounded-3 my-3 ms-2 w-25" alt="Icone">
-        <div class="text-center my-auto ms-3 lead text-light">
-            <?php echo $_SESSION['nome'];?>
-        </div>
+    
+        <?php
+            if ($_SESSION['nome'] == 'Diego') {
+                echo '<div class="text-center my-auto ms-3 my-3 lead text-light">';
+                    echo "<h4 class='fw-semibold'>".$_SESSION['nome']."</h4>\n";
+                    echo '<img src="../img/diego.png" class="rounded-3 ms-2 w-75" alt="Icone">';
+                echo '</div>';
+            } else {
+                echo '<div class="text-start my-auto ms-3 my-3 lead text-light">';
+                    echo '<img src="../img/icon.png" class="rounded-3 my-3 mx-3 w-25" alt="Icone">';
+                    echo $_SESSION['nome'];
+                echo '</div>';
+            }
+        ?>
     </div>
 </div>
