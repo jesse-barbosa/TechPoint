d211f1c777c033c5ee519ff1dee780c2b1e12767
jesse-barbosa <barbosajesse419@gmail.com>
2024-09-17 08:54:28 -0300

    Resolvendo conflitos

--- admin/classe/listarImagens.php
+++ admin/classe/listarImagens.php
@@ -2,26 +2,18 @@
 include_once("MinhaConexao.php");
 
 class ListarImagens extends MinhaConexao {
-    public function listarImagens(): array
-    {
-        try {
-            $sql = "SELECT idImage, nameImage, urlImage FROM images WHERE deletedImage = 0";
-            $resultado = self::execSql($sql);
-    
-            if (mysqli_num_rows($resultado) > 0) {
-                $imagens = [];
-                while ($row = mysqli_fetch_assoc($resultado)) {
-                    $imagens[] = $row;
-                }
-                return $imagens;
-            } else {
-                return [];
-            }
-        } catch (Exception $e) {
-            echo "Erro ao listar imagens: " . $e->getMessage();
-            return [];
-        }
+
+    public function __construct() {
+        parent::__construct();
     }
-    
+
+    public function listarImagens($typeImage) {
+        $sql = "SELECT idImage, nameImage FROM images WHERE typeImage = ? AND deletedImage = 0";
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->bind_param("s", $typeImage);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        return $resultado->fetch_all(MYSQLI_ASSOC);
+    }
 }
 ?>
